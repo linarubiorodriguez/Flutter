e6a93f9371b8116a34f12dbbae5f36538e03f87c
“Lina” <linarubio28987@gmail.com>
2025-04-01 16:17:58 -0500

    Plantilla inicial

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bienvenido',
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Inicio")),
+      body: const Center(child: Text("Bienvenido a la app")),
+    );
+  }
+}
